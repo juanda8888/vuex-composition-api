641d42299db4cfe854510a6b267295655e67cf6a
juanda8888 <juandavid.parra88@gmail.com>
2021-09-15 21:07:12 +0200

    added fetch api and vuex

---
+++ src/components/CardList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row">
+      <div class="col-12">
+          {{paises}}
+      </div>
+  </div>
+</template>
+
+<script>
+import { computed, onMounted } from '@vue/runtime-core'
+import { useStore } from 'vuex'
+export default {
+  setup() {
+    const store = useStore()
+    const paises = computed(() => {
+      return store.state.paises
+    })
+    onMounted(() => {
+      store.dispatch('getPaises')
+    })
+
+    return {paises}
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,27 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    paises: [],
+    paisesFiltrados: []
   },
   mutations: {
+    setPaises(state, payload) {
+      state.paises = payload
+    },
+    setPaisesFiltrados(state, payload) {
+      state.paisesFiltrados = payload
+    }
   },
   actions: {
+    async getPaises({ commit }) {
+      try {
+        const res = await fetch('api.json')
+        const data = await res.json()
+        commit('setPaises', data)
+      } catch (error) {
+        
+      }
+    }
   },
   modules: {
   }
